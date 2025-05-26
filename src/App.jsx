2d56607f5be943d4e736a3866bc624dcfc8249b7
YouKnowWhoIs <leokatmax@gmail.com>
2025-05-26 14:30:32 +0300

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./style/App.css";
 import UserList from "./component/userList/UserList.jsx";
+import Counter from "./component/counter/counter.jsx";
 
 function App() {
   return (
     <>
       <h1>Practice CSS/React 1</h1>
-      <UserList />;
+      <UserList />
+      <Counter />
     </>
   );
 }
---
+++ src/component/counter/Counter.jsx
@@ -0,0 +1,32 @@
+import "../../style/counter/Counter.css";
+import { useState } from "react";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonPlus() {
+    setCounter(counter + 1);
+  }
+
+  function handleButtoтMinus() {
+    if (counter < 1) return;
+    setCounter(counter - 1);
+  }
+
+  return (
+    <div className="counterItems">
+      <p className="counterTest">Звичайний лічильник, не меньше 0</p>
+      <div className="counterContainer">
+        <button type="button" onClick={handleButtonPlus}>
+          +
+        </button>
+        <p>{counter}</p>
+        <button type="button" onClick={handleButtoтMinus}>
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
